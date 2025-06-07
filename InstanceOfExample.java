6ac9e153d70e5d9adaaa9dd1b7d98745b76c0bc1
Charleskojomark <markcharleskojo@gmail.com>
2025-06-07 01:15:52 +0100

    instanceof

---
+++ InstanceOfExample.java
@@ -0,0 +1,9 @@
+public class InstanceOfExample {
+    public static void main(String[] args) {
+        Customer k1 = new Customer();
+
+        boolean e7 = k1 instanceof Customer;
+
+        System.out.println("k1 instanceof Customer: " + e7);
+    }
+}
